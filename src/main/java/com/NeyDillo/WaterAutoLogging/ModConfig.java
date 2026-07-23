c51f5e07c76a0f2403e06e0eb24323b7ae2d9594
NeyDillo <Honey_Armadillo_2025@outlook.com>
2026-07-23 23:41:38 +0800

    Delete src/main directory

--- src/main/java/com/NeyDillo/WaterAutoLogging/ModConfig.java
+++
@@ -1,6 +0,0 @@
-package com.NeyDillo.WaterAutoLogging;
-public class ModConfig {
-    public String password = "";
-    public boolean autoFly = false;
-    public boolean ignoreJavaWarning = false;
-}
