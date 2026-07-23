f2d5ea161909c8a2e76b15e93e90e0c8d0858306
NeyDillo <Honey_Armadillo_2025@outlook.com>
2026-07-23 23:42:40 +0800

    Add files via upload

---
+++ src/main/java/com/NeyDillo/WaterAutoLogging/ModConfig.java
@@ -0,0 +1,6 @@
+package com.NeyDillo.WaterAutoLogging;
+public class ModConfig {
+    public String password = "";
+    public boolean autoFly = false;
+    public boolean ignoreJavaWarning = false;
+}
